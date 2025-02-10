570ece5cabb274a5b168321e9ae43b7f55c0e256
SuhaniKapasiya <107610361+SuhaniKapasiya@users.noreply.github.com>
2025-02-10 18:39:40 +0530

    move  all folder code in src folder

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const connectDB = require("../config/database");
+const connectDB = require("./config/database");
 const cookieParser = require("cookie-parser");
 // const User = require("./models/user");
 
